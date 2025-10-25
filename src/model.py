bafef2ab38f50c39789db91e40050cfd7c348910
bryanTRX <bryanalexandre03@gmail.com>
2025-10-25 16:30:46 -0400

    Added logic for Augmentation of image

--- src/model.py
+++ src/model.py
@@ -3,12 +3,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class EmotionCNN(nn.Module):
-    def __init__(self):
+    def __init__(self, num_classes: int = 7):
         super(EmotionCNN, self).__init__()
         
         self.conv_layers = self._build_conv_layers()
         
-        self.fc_layers = self._build_fc_layers()
+        self.fc_layers = self._build_fc_layers(num_classes)
 
     def _build_conv_layers(self):
         layers = nn.Sequential(
@@ -23,23 +23,21 @@ class EmotionCNN(nn.Module):
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.ReLU(),
             
-            nn.BatchNorm2d(128),
-            nn.Dropout(0.25),
-            
+            nn.BatchNorm2d(128),            
             nn.MaxPool2d(2,2)
         )
         
         return layers
     
-    def _build_fc_layers(self):
+    def _build_fc_layers(self, num_classes: int = 7):
         layers = nn.Sequential(
             nn.Flatten(),
             
             nn.Linear(128*12*12, 128),
             nn.ReLU(),
-            nn.Dropout(0.35),
+            nn.Dropout(0.25),
             
-            nn.Linear(128, 7)
+            nn.Linear(128, num_classes)
         )
         
         return layers
